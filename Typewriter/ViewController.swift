7f4dfc9542b20b43f73e516c7b8a960806276f0d
Alex Radford <alex.radford@byos.io>
2026-07-22 07:54:37 -0300

    Initial Commit

---
+++ Typewriter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Typewriter
+//
+//  Created by Alex Radford on 2026-07-22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
